468dcac55155260308f549b091f80188879fe3d1
fnavizq355 <132046114+fnavizq355@users.noreply.github.com>
2023-05-01 10:50:07 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,19 @@
+function puntuacion () {
+    if (x == coche) {
+        juega = false
+    } else {
+        puntos = puntos + 1
+        espera = espera - 25
+        led.unplot(x, y)
+    }
+}
+function final () {
+    basic.clearScreen()
+    basic.pause(100)
+    basic.showNumber(puntos)
+    basic.pause(500)
+    basic.clearScreen()
+}
 input.onButtonPressed(Button.A, function () {
     if (juega && coche > 1) {
         led.unplot(coche, 4)
@@ -5,15 +21,53 @@ input.onButtonPressed(Button.A, function () {
         led.plot(coche, 4)
     }
 })
-input.onButtonPressed(Button.AB, function () {
-    espera = 500
-    coche = 2
+function trafico () {
+    x = randint(1, 3)
+    y = 0
+    while (y <= 4) {
+        carretera()
+        led.plot(x, y)
+        basic.pause(espera)
+        led.unplot(x, y)
+        y = y + 1
+    }
+}
+function carretera () {
+    led.toggle(0, 0)
+    led.toggle(4, 0)
+    led.toggle(0, 1)
+    led.toggle(4, 1)
+    led.toggle(0, 2)
+    led.toggle(4, 2)
+    led.toggle(0, 3)
+    led.toggle(4, 3)
+    led.toggle(0, 4)
+    led.toggle(4, 4)
+}
+function inicio () {
     basic.showNumber(3)
     basic.showNumber(2)
     basic.showNumber(1)
-    basic.clearScreen()
+    basic.showLeds(`
+        . . . . .
+        # . . . #
+        . . . . .
+        # . . . #
+        . . . . .
+        `)
     led.plot(coche, 4)
+}
+input.onButtonPressed(Button.AB, function () {
+    puntos = 0
+    espera = 400
+    coche = 2
     juega = true
+    inicio()
+    while (juega) {
+        trafico()
+        puntuacion()
+    }
+    final()
 })
 input.onButtonPressed(Button.B, function () {
     if (juega && coche < 3) {
@@ -22,34 +76,10 @@ input.onButtonPressed(Button.B, function () {
         led.plot(coche, 4)
     }
 })
-let puntos = 0
 let y = 0
-let x = 0
 let espera = 0
+let puntos = 0
 let coche = 0
+let x = 0
 let juega = false
 juega = false
-basic.forever(function () {
-    if (juega) {
-        x = randint(1, 3)
-        y = 0
-        led.plot(x, y)
-        for (let index = 0; index < 4; index++) {
-            basic.pause(espera)
-            led.unplot(x, y)
-            y = y + 1
-            led.plot(x, y)
-        }
-        if (x == coche && y == 4) {
-            juega = false
-            basic.showNumber(puntos)
-            basic.pause(500)
-            basic.clearScreen()
-        } else {
-            puntos = puntos + 1
-            espera = espera - 25
-            led.unplot(x, y)
-            basic.pause(espera)
-        }
-    }
-})
